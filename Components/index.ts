de62df2dfdf450d8b8bdf2f288af588b23e97357
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-11-13 03:31:04 +0700

    object-create

--- Components/index.ts
+++ Components/index.ts
@@ -4,8 +4,23 @@ function Home(a:number, b:number):number {
 
 // console.log(Home(10,10));
 
-function map(nums) {
-    return nums.map(num => {num +1})
+function map(nums: number[]) :number[] {
+    return nums.map(num => num +1);
 }
 
-console.log(map([1,2,3]));
+// console.log(map([1,2,3]));
+
+interface Person {
+    name: string;
+    age?: number;
+}
+
+type User =  {
+    name: string;
+    age?: number;
+}
+
+const alice: Person = {name: "Alice", age: 12};
+const bobo: Person = {name: "Bobo"};
+const tt:User = {name: "Ttt", age: 12};
+console.log(alice,bobo,tt);
