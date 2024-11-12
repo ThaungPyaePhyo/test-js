ef5d07556477c3c085ce6c1ed7644f31893012b0
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-11-13 03:40:48 +0700

    generic

--- Components/index.ts
+++ Components/index.ts
@@ -23,4 +23,26 @@ type User =  {
 const alice: Person = {name: "Alice", age: 12};
 const bobo: Person = {name: "Bobo"};
 const tt:User = {name: "Ttt", age: 12};
-console.log(alice,bobo,tt);
+// console.log(alice,bobo,tt);
+
+function wrap<T>(value: T): T[] {
+    return [value];
+}
+
+const hello = wrap('hello');
+const num = wrap(123);
+// console.log(num,hello);
+
+type Res<A> = {
+    data : A,
+    status: number
+}
+
+const user: Res<User> = {
+    data: {
+        name: 'Ma Ma',
+        age: 20
+    },
+    status: 200
+}
+console.log(user);
