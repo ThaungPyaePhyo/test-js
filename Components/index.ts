cfab97e5d2e97c0f048da72db609ec475070c4af
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-11-13 01:53:50 +0700

    first

---
+++ Components/index.ts
@@ -0,0 +1,11 @@
+function Home(a:number, b:number):number {
+    return a + b;
+}
+
+// console.log(Home(10,10));
+
+function map(nums) {
+    return nums.map(num => {num +1})
+}
+
+console.log(map([1,2,3]));
